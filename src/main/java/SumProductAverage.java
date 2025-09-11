3356a3bba603ceb67b93519490bd5d720c6cddc3
99d71fec-be3c-4816-a7fb-77db311dfd9e <03george.haan@kamsc.org>
2025-09-11 14:00:42 -0400

    done

--- src/main/java/SumProductAverage.java
+++ src/main/java/SumProductAverage.java
@@ -15,8 +15,25 @@
 
 import java.util.Scanner;
 
-public class SumProductAverage {
-    public static void main(String[] args) {
-        // TODO
+public class SumProductAverage
+{
+    public static void main(String[] args)
+    {
+        double x, y, z, numruns, i;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many numbers? ");
+        numruns = scan.nextInt();
+        y = 0.0;
+        z = 1.0;
+        for(i = 0.0; i < numruns; i++)
+        {
+            System.out.println("Enter a number ");
+            x = scan.nextDouble();
+            y = y + x;
+            z = z * x;
+        }
+        System.out.println("\nThe sum is " + y);
+        System.out.println("\nThe average is " + y/numruns);
+        System.out.println("\nThe product is " + z);
     }
 }
